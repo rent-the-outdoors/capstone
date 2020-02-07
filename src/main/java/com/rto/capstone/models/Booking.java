746dca5b677257e9b3d09d893a359e407c73b3ff
Nico Virgino <nicovirgino@gmail.com>
2020-02-07 13:36:57 -0600

    working on booking

---
+++ src/main/java/com/rto/capstone/models/Booking.java
@@ -0,0 +1,7 @@
+package com.rto.capstone.models;
+
+public class Booking {
+    private String date;
+    private String address;
+
+}
--- src/main/java/com/rto/capstone/repositories/UserRepository.java
+++ src/main/java/com/rto/capstone/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.rto.capstone.repositories;
 
-public interface UserRepository {
+import com.rto.capstone.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
