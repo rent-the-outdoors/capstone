2de5d28923770df90b5be4ebf1c62ea6cf2ee01a
Jacob Barrow <jacob.barrow@gmail.com>
2020-02-07 16:00:00 -0600

    stripe service api in progress

--- src/main/java/com/rto/capstone/controllers/CheckoutController.java
+++ src/main/java/com/rto/capstone/controllers/CheckoutController.java
@@ -4,8 +4,21 @@ import com.rto.capstone.models.Place;
 import com.rto.capstone.models.User;
 import com.rto.capstone.repositories.PlaceRepository;
 import com.rto.capstone.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+//import capstone.site.Services.StripeService;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class CheckoutController {
@@ -21,5 +34,45 @@ public class CheckoutController {
     public String confirmBooking() {
         return "/views/confirmation";
     }
+
+
+
+        @Value("${STRIPE_PUBLIC_KEY}")
+        private String stripePublicKey;
+
+        @GetMapping("/bookNow/{id}/checkout")
+        public String checkout(Model model, @PathVariable Integer id) {
+//            model.addAttribute(schoolClassDao.getOne(id));
+//            model.addAttribute("amount", schoolClassDao.getOne(id).getPrice() * 100);
+            model.addAttribute("amount", 100);
+            model.addAttribute("stripePublicKey", stripePublicKey);
+            return "/pages/checkout";
+        }
+//        @Autowired
+//        private StripeService stripeService;
+
+        @RequestMapping(value = "/charge/{id}", method = RequestMethod.POST)
+        public String chargeCard(@PathVariable Integer id, Model model, HttpServletRequest request) throws Exception {
+//            model.addAttribute("class", schoolClassDao.getOne(id));
+            String token = request.getParameter("stripeToken");
+            Double amount = Double.parseDouble(request.getParameter("amount"));
+            model.addAttribute("amount", amount);
+            stripeService.chargeNewCard(token, amount);
+            return "/pages/result";
+        }
+
+        @GetMapping("/result")
+        @ResponseBody
+        public String yes() {
+            return "thats right";
+        }
+
+    }
+
+
+
+
+
+
 
 }
---
+++ src/main/java/com/rto/capstone/services/StripeService.java
@@ -0,0 +1,43 @@
+package com.rto.capstone.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.stripe.Stripe;
+import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+    @Service
+    public class StripeService {
+
+        @Value("${STRIPE_SECRET_KEY}")
+        private String API_SECRET_KEY;
+
+        @Autowired
+        public StripeService() {
+            //find a way to hide this
+            Stripe.apiKey = "sk_test_fcaJ2PuAc7DCFWwIMFTyH9kf004p4PEHU8";
+        }
+
+        public Charge chargeNewCard(String token, double amount) throws Exception {
+            Map<String, Object> chargeParams = new HashMap<>();
+            chargeParams.put("amount",(int)(amount * 100));
+            chargeParams.put("currency", "USD");
+            chargeParams.put("source", token);
+            return Charge.create(chargeParams);
+        }
+    }
+
+
+
+
+
