e40a1230e39bf73c9c3fc85bdc615408345f65aa
MrOffer <thomas.e.offer@gmail.com>
2020-02-07 11:35:45 -0600

    home

---
+++ src/main/java/com/rto/capstone/controllers/homeController.java
@@ -0,0 +1,15 @@
+package com.rto.capstone.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class homeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String homeController(){
+        return "Hello Dumbasses!";
+    }
+}
