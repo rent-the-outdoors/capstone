e45c9bcd915a0e19da0a582721d43a5343b8be88
MrOffer <thomas.e.offer@gmail.com>
2020-02-08 10:56:40 -0600

    added calendar form

--- src/main/java/com/rto/capstone/controllers/CalendarController.java
+++ src/main/java/com/rto/capstone/controllers/CalendarController.java
@@ -2,6 +2,8 @@ package com.rto.capstone.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CalendarController {
@@ -9,4 +11,11 @@ public class CalendarController {
      public String CalendarController(){
          return "views/calendar";
      }
+
+    @PostMapping("/calendar")
+    private String addDates(){
+
+
+         return "views/confirmation";
+    }
 }
