6ca850f265e49a01c8c6a44e53af2737634d7572
Nico Virgino <nicovirgino@gmail.com>
2020-02-11 11:34:41 -0600

    ui for places

--- src/main/resources/db/rto_db.sql
+++ src/main/resources/db/rto_db.sql
@@ -1,6 +1,14 @@
 -- USE rto_db;
 -- INSERT INTO activities(activity) VALUES ('hunting'), ('fishing'), ('boating'), ('camping');
 
+use rto_db;
 
+
+
+INSERT INTO places(address, cost_per_day, description, title)
+VALUES
+("9898 Colonnade San Antonio, TX", "$500.00", "my apartment is for rent!", "Nico's apartmnt"),
+("Pearl Pkwy, San Antonio, TX 78215", "$5000.00", "the pearl is for rent", "The Pearl"),
+("Seguin, TX", "$5000.00", "my city of seguin is for rent!", "Seguin")
 
 
