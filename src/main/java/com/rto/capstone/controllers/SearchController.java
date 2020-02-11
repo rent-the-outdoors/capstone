e3eccba7a2991fc5e8fb7ffe83ce9d1a2dffb186
MrOffer <thomas.e.offer@gmail.com>
2020-02-10 19:33:32 -0600

    fixed search controller

--- src/main/java/com/rto/capstone/controllers/SearchController.java
+++ src/main/java/com/rto/capstone/controllers/SearchController.java
@@ -1,13 +1,13 @@
-package com.rto.capstone.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class SearchController {
-    @GetMapping("/search")
-    public String searchResults(@RequestParam  ) {
-        return ("/views/search");
-    }
-}
+//package com.rto.capstone.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//public class SearchController {
+//    @GetMapping("/search")
+//    public String searchResults(@RequestParam  ) {
+//        return ("/views/search");
+//    }
+//}
