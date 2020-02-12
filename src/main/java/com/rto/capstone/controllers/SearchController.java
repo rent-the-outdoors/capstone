f762482d551992f2a63ebaad2ad70e59b57eef99
MrOffer <thomas.e.offer@gmail.com>
2020-02-12 13:34:46 -0600

    debugging

--- src/main/java/com/rto/capstone/controllers/SearchController.java
+++ src/main/java/com/rto/capstone/controllers/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -29,23 +30,31 @@ public class SearchController {
 
 
     @GetMapping("/search")
-    public String searchResults(@RequestParam String search, @RequestParam (required = false) String huntCheck, @RequestParam (required=false) String fishCheck, @RequestParam (required=false) String campCheck, @RequestParam (required=false) String boatCheck, Model model) {
-        List<Place> checkList = new ArrayList<>();
-        List<Place> allPlaces = placesDao.findAll();
-        for (Place place : allPlaces) {
-            if (huntCheck != null && place.getDescription().contains(huntCheck)
-                    || (fishCheck != null && place.getDescription().contains(fishCheck))
-                    || (campCheck != null && place.getDescription().contains(campCheck))
-                    || (boatCheck != null && place.getDescription().contains(boatCheck))
-                    || (search != null && (place.getDescription().contains(search)
-                    || place.getTitle().contains(search)
-                    || place.getAddress().contains(search)))) {
-                checkList.add(place);
-
-            }
-        }
-        model.addAttribute("allPlaces", allPlaces);
-        model.addAttribute("searchQuery", checkList);
-        return ("/views/search");
+    @ResponseBody
+    public String boo() {
+        return "nope";
     }
+
+//    public String searchResults(@RequestParam String search, @RequestParam (required = false) String huntCheck, @RequestParam (required=false) String fishCheck, @RequestParam (required=false) String campCheck, @RequestParam (required=false) String boatCheck, Model model) {
+//        List<Place> checkList = new ArrayList<>();
+//        List<Place> allPlaces = placesDao.findAll();
+//        if (allPlaces == null) {
+//            return "Null!";
+//        }
+//        for (Place place : allPlaces) {
+//            if (huntCheck != null && place.getDescription().contains(huntCheck)
+//                    || (fishCheck != null && place.getDescription().contains(fishCheck))
+//                    || (campCheck != null && place.getDescription().contains(campCheck))
+//                    || (boatCheck != null && place.getDescription().contains(boatCheck))
+//                    || (search != null && (place.getDescription().contains(search)
+//                    || place.getTitle().contains(search)
+//                    || place.getAddress().contains(search)))) {
+//                checkList.add(place);
+//
+//            }
+//        }
+//        model.addAttribute("allPlaces", allPlaces);
+//        model.addAttribute("searchQuery", checkList);
+
+
 }
