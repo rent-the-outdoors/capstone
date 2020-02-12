bf1f3ad1e080b683c559b6fc6c25fdb4c6415723
MrOffer <thomas.e.offer@gmail.com>
2020-02-12 12:58:25 -0600

    fixed error

--- src/main/java/com/rto/capstone/controllers/SearchController.java
+++ src/main/java/com/rto/capstone/controllers/SearchController.java
@@ -41,14 +41,14 @@ public class SearchController {
                     || place.getTitle().contains(search)
                     || place.getAddress().contains(search)))) {
                 checkList.add(place);
-                String shmimagePath = imageDao.getOne(1L).getImagePath();
-                model.addAttribute("place", place);
-                model.addAttribute("imagePath", imageDao.getOne(1L).getImagePath());
+//                String shmimagePath = imageDao.getOne(1L).getImagePath();
+//                model.addAttribute("place", place);
+//                model.addAttribute("imagePath", imageDao.getOne(1L).getImagePath());
 
             }
         }
         model.addAttribute("allPlaces", allPlaces);
         model.addAttribute("searchQuery", checkList);
-        return ("views/search");
+        return ("/views/search");
     }
 }
