fc32d5632ad86e27362f02b8efa2ca29adbc736c
Jacob Barrow <jacob.barrow@gmail.com>
2020-02-13 11:57:22 -0600

    single place picture alignment

--- src/main/resources/db/rto_db.sql
+++ src/main/resources/db/rto_db.sql
@@ -3,7 +3,15 @@
 
 
 USE rto_db;
-INSERT INTO images(image_path, place_id) VALUES ('img/hunt_icon.png', 1);
+TRUNCATE images;
+INSERT INTO images(image_path, place_id) VALUES ('/img/hunt_icon.png', 1),
+                                                ('/img/adventure-bg.jpg', 1),
+                                                ('/img/adventure-bg.jpg', 1),
+                                                ('/img/adventure-bg.jpg', 1),
+                                                ('/img/adventure-bg.jpg', 1),
+                                                ('/img/adventure-bg.jpg', 1);
+
+
 
 
 
