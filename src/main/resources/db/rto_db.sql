8fe8879121ad05b9f1b1d813e688513ea947e8dd
Jacob Barrow <jacob.barrow@gmail.com>
2020-02-14 06:51:55 -0600

    fixed database constraint

--- src/main/resources/db/rto_db.sql
+++ src/main/resources/db/rto_db.sql
@@ -1,19 +1,24 @@
 -- USE rto_db;
 -- INSERT INTO activities(activity) VALUES ('hunting'), ('fishing'), ('boating'), ('camping');
 
-
+DROP DATABASE rto_db;
 USE rto_db;
+TRUNCATE reviews;
+TRUNCATE users;
 TRUNCATE images;
-INSERT INTO images(image_path, place_id) VALUES ('/img/hunt_icon.png', 1),
-                                                ('/img/adventure-bg.jpg', 1),
-                                                ('/img/adventure-bg.jpg', 1),
-                                                ('/img/adventure-bg.jpg', 1),
-                                                ('/img/adventure-bg.jpg', 1),
-                                                ('/img/adventure-bg.jpg', 1);
+INSERT INTO images(image_path, place_id) VALUES ('/img/adventure-calm-clouds-dawn-414171.jpg', 1),
+                                                ('/img/flock-of-birds-917494.jpg', 1),
+                                                ('/img/green-trees-near-calm-body-of-water-2275221.jpg', 1),
+                                                ('/img/camping.jpg', 1),
+                                                ('/img/rockclimbing.jpeg', 1),
+                                                ('/img/two-men-fishing-on-lake-1630039.jpg', 1);
 
 
 
 
+INSERT INTO users(email, first_name, last_name, image_path, username, password, phone_num)
+VALUES
+('banana@gmail.com', 'Ba', 'Nana', null, 'banana', 'password', '210-273-5613');
 
 
 INSERT INTO places(address, cost_per_day, description, title)
@@ -22,8 +27,5 @@ VALUES
 ('Pearl Pkwy, San Antonio, TX 78215', '5000.00', 'the pearl is for rent', 'The Pearl'),
 ('Seguin, TX', '5000.00', 'my city of seguin is for rent!', 'Seguin');
 
-INSERT INTO users(email, first_name, last_name, image_path, username, password, phone_num)
-VALUES
-('banana@gmail.com', 'Ba', 'Nana', null, 'banana', 'password', '210-273-5613');
 
 
