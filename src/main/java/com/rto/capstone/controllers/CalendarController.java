b7ab75455d3e152033f28c820be3f60bc5ba83a3
Nico Virgino <nicovirgino@gmail.com>
2020-02-17 20:45:29 -0600

    calendar api, work in progress, figured out how to get data, and now need to post it to db

--- src/main/java/com/rto/capstone/controllers/CalendarController.java
+++ src/main/java/com/rto/capstone/controllers/CalendarController.java
@@ -1,8 +1,13 @@
 package com.rto.capstone.controllers;
 
+import com.google.gson.Gson;
+import com.rto.capstone.models.Booking;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class CalendarController {
@@ -10,6 +15,26 @@ public class CalendarController {
      @GetMapping("/calendar")
      public String CalendarController(){
          return "views/calendar";
+     }
+
+     @RequestMapping(value = "/schedule/getBooking", method = RequestMethod.GET)
+     public
+     @ResponseBody
+     String getBooking(HttpServletResponse res) {
+         Map<String, Object> map = new HashMap<String, Object>();
+         Booking booking = new Booking();
+         map.put("id", booking.getId());
+         map.put("start", booking.getDateStart());
+         map.put("end", booking.getDateEnd());
+
+         // Convert to JSON string.
+         String json = new Gson().toJson(map);
+
+         // write json string
+         res.setContentType("application/json");
+         res.setCharacterEncoding("UTF-8");
+
+         return json;
      }
 
     @PostMapping("/calendar")
