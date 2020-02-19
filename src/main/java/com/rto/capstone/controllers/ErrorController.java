2e67ad78a577e109d1fd304a7a1312aa1b7a5ed9
MrOffer <thomas.e.offer@gmail.com>
2020-02-19 07:43:08 -0600

    error page

--- src/main/java/com/rto/capstone/controllers/ErrorController.java
+++ src/main/java/com/rto/capstone/controllers/ErrorController.java
@@ -1,1 +1,17 @@
+package com.rto.capstone.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+
+    @GetMapping("/error")
+    public String ErrorController() {
+        return "views/error";
+    }
+
+
+}
