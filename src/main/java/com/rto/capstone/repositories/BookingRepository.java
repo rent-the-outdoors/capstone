9174ad2c2cf437687cca094705fb8daa074eca8e
Nico Virgino <nicovirgino@gmail.com>
2020-02-19 11:07:03 -0600

    all the calendar stuff in the land

--- src/main/java/com/rto/capstone/repositories/BookingRepository.java
+++ src/main/java/com/rto/capstone/repositories/BookingRepository.java
@@ -3,5 +3,8 @@ package com.rto.capstone.repositories;
 import com.rto.capstone.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
 }
