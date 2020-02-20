86177ace1cfbee0b5b1563bc7c96de28e02ed8d8
Jacob Barrow <jacob.barrow@gmail.com>
2020-02-20 13:01:10 -0600

    fixed the filestack upload

--- src/main/java/com/rto/capstone/controllers/PlaceController.java
+++ src/main/java/com/rto/capstone/controllers/PlaceController.java
@@ -58,7 +58,7 @@ public class  PlaceController {
 
     //Create place POST
     @PostMapping(path = "/places/{id}/create")
-    public String createAndPostFormForPlaceWithInfoFromGet(@RequestParam(required=false) String image_path2, @RequestParam User userId, @PathVariable Long id, @RequestParam String description, @RequestParam String title, @RequestParam String cost, @RequestParam String address, @RequestParam(required=false) String image_path4, @RequestParam(required=false) String image_path6, @RequestParam(required=false) String image_path8, @RequestParam(required=false) String image_path10  )
+    public String createAndPostFormForPlaceWithInfoFromGet(@RequestParam(required=false) String image_path1, @RequestParam User userId, @PathVariable Long id, @RequestParam String description, @RequestParam String title, @RequestParam String cost, @RequestParam String address, @RequestParam(required=false) String image_path2, @RequestParam(required=false) String image_path3, @RequestParam(required=false) String image_path4, @RequestParam(required=false) String image_path5  )
 
 //
     {
@@ -76,26 +76,31 @@ public class  PlaceController {
         PlaceImage placeImage4 = new PlaceImage();
         PlaceImage placeImage5 = new PlaceImage();
         //set image path on new placeImage
-        if (image_path2 == null) {
-            image_path4 = "http://via.placeholder/640x360";
+        if (image_path1 != null) {
+        placeImage.setImagePath(image_path1);
+        } else {
+            image_path1 = "http://via.placeholder.com/640x360";
         }
-        placeImage.setImagePath(image_path2);
-        if (image_path4 == null) {
+        if (image_path2 != null) {
+            placeImage2.setImagePath(image_path2);
+        } else {
+            image_path2 = "http://via.placeholder.com/640x360";
+        }
+        if (image_path3 != null) {
+            placeImage3.setImagePath(image_path3);
+        } else {
+            image_path3 = "http://via.placeholder.com/640x360";
+        }
+        if (image_path4 != null) {
+            placeImage4.setImagePath(image_path4);
+        } else {
             image_path4 = "http://via.placeholder.com/640x360";
         }
-        placeImage2.setImagePath(image_path4);
-        if (image_path6 == null) {
-            image_path6 = "http://via.placeholder.com/640x360";
+        if (image_path5 != null) {
+            placeImage5.setImagePath(image_path5);
+        } else {
+            image_path5 = "http://via.placeholder.com/640x360";
         }
-        placeImage3.setImagePath(image_path6);
-        if (image_path8 == null) {
-            image_path8 = "http://via.placeholder.com/640x360";
-        }
-        placeImage4.setImagePath(image_path8);
-        if (image_path10 == null) {
-            image_path10 = "http://via.placeholder.com/640x360";
-        }
-        placeImage5.setImagePath(image_path10);
 
         //save new placeImage into image table
         imagesDao.save(placeImage);
