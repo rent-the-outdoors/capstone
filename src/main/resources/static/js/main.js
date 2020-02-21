dd06ee59e4086dd726babeaffd05fea6eb7eb406
Nico Virgino <nicovirgino@gmail.com>
2020-02-21 13:21:42 -0600

    more ui

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,22 +1,22 @@
-// "use strict";
-//
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
-//
-// // Get the search
-// var hero = document.getElementById("search");
-//
-// // Get the offset position of the search
-// var sticky = hero.offsetTop;
-//
-// // Add the sticky class to the search when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//     if (window.pageYOffset >= sticky) {
-//         hero.classList.add("sticky")
-//     } else {
-//         hero.classList.remove("sticky");
-//     }
-// }
+"use strict";
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the search
+var hero = document.getElementById("search");
+
+// Get the offset position of the search
+var sticky = hero.offsetTop;
+
+// Add the sticky class to the search when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        hero.classList.add("sticky")
+    } else {
+        hero.classList.remove("sticky");
+    }
+}
 //
 // $('#closeModal').click(function () {
 //     $('#bookModal').toggleClass("is-active")
